fbe6f0180e18e78007cdd1832dec93bfb729e2c9
Pedro-developeer <79731855+Pedro-developeer@users.noreply.github.com>
2021-03-01 14:30:49 -0300

    Imprimindo Variáveis no Java

---
+++ ImprimindoVariaveis.java
@@ -0,0 +1,7 @@
+public class ImprimindoVariaveis  {
+    public static void main(String[] args){
+        int idadeDosPaisParaCadastro = 10;
+            System.out.println("A idade dos pais é:"+idadeDosPaisParaCadastro);
+
+    }
+}
