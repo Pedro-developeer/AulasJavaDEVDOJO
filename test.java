d24a2aa0dc8f64408c15382dffd3c88919754c2a
Pedro-developeer <79731855+Pedro-developeer@users.noreply.github.com>
2021-03-01 15:04:22 -0300

    Atividade
    Crie um algorítimo que possua variáveis para salvar os seguintes dados: Nome, Endereço, Telefone de uma pessoa.

---
+++ test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String nome = "Pedro";
+        String endereco = "Rua Panamá";
+        String telefone = "984737305";
+                System.out.println ("O "+nome+" domiciliando no endereço "+endereco+" e telefone "+telefone+" não possui nenhum tipo de pendencia ");
+    }
+}
