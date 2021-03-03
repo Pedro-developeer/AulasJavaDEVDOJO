4ce4e3a5772bee92145df6dfa50b38e9625595cb
Pedro Henrique <79731855+Pedro-developeer@users.noreply.github.com>
2021-03-03 11:26:34 -0300

    Atividade de Operadores de Atribuição

---
+++ OperadoresDeAtribuicao.java
@@ -0,0 +1,10 @@
+public class OperadoresDeAtribuicao {
+    public static void main(String[] args) {
+        int salario = 1800;
+       // salario = salario + 1000;
+       int salario = 1800;
+       int numero = 11 % 2;
+       numero = numero % 2;
+        System.out.println(numero);
+    }
+}
